4267cec2ef90ac20eb59c97c1f712db17a1ad241
Rhythm Girdhar <ridhigirdhar3@gmail.com>
2023-04-30 01:16:55 -0700

    Sixth Attempt: 0.9821882472849149

--- code/competition.py
+++ code/competition.py
@@ -11,73 +11,6 @@ def write_csv_file(result_str, output_file):
         f.writelines(result_header)
         f.writelines(result_str)
 
-def calculateWeightedAverage(weight_list_candidates):
-    X, Y = 0, 0
-    for weight, rating in weight_list_candidates:
-        X += weight * rating
-        Y += abs(weight)
-    return 3.5 if Y == 0 else X / Y
-
-def computeSimilarity(co_rated_users, dict1, dict2):
-    val1_list, val2_list = list(), list()
-
-    [(val1_list.append(float(dict1[user_id])),
-      val2_list.append(float(dict2[user_id]))) for user_id in co_rated_users]
-
-    avg1 = sum(val1_list) / len(val1_list)
-    avg2 = sum(val2_list) / len(val2_list)
-
-    numerator = sum(map(lambda pair: (pair[0] - avg1) * (pair[1] - avg2), zip(val1_list, val2_list)))
-
-    if numerator == 0:
-        return 0
-        
-    denominator = math.sqrt(sum(map(lambda val: (val - avg1) ** 2, val1_list))) * \
-                  math.sqrt(sum(map(lambda val: (val - avg2) ** 2, val2_list)))
-    if denominator == 0:
-        return 0
-
-    return numerator / denominator
-
-def item_based_cf(business, user):
-    if (business not in business_user_dict) and (user not in user_business_dict):
-        return DEFAULT_VALUE
-
-    if business not in business_user_dict:
-        return user_avg_rating[user]
-    
-    if user not in user_business_dict:
-        return business_avg_rating[business]
-    
-    weight_list = list()
-
-    for bus in user_business_dict[user]:
-        business_bus = tuple(sorted((business, bus)))
-        if business_bus in weight_dict:
-            weight = weight_dict[business_bus]
-        else:
-            co_rated_users = list(set(business_user_dict[business]) & (set(business_user_dict[bus])))
-            no_co_rated_users = len(co_rated_users)
-            if no_co_rated_users <= 1:
-                weight = (5.0 - abs(business_avg_rating[business] - business_avg_rating[bus])) / 5
-            elif no_co_rated_users == 2:
-                weight_1 = (5.0 - abs(float(bus_user_r_dict[business][co_rated_users[0]]) - float(bus_user_r_dict[bus][co_rated_users[0]]))) / 5
-                weight_2 = (5.0 - abs(float(bus_user_r_dict[business][co_rated_users[1]]) - float(bus_user_r_dict[bus][co_rated_users[1]]))) / 5
-                weight = (weight_1 + weight_2) / 2
-            else:
-                weight = computeSimilarity(co_rated_users, bus_user_r_dict[business], bus_user_r_dict[bus])
-            
-            weight_dict[business_bus] = weight
-        
-        if weight > 0:
-            weight_list.append((weight, float(bus_user_r_dict[bus][user])))
-
-    weight_list_candidates = sorted(weight_list, key = lambda x: -x[0])[:NEIGHBORS]
-
-    prediction = calculateWeightedAverage(weight_list_candidates)
-
-    return prediction
-
 def createXandY(data_RDD):
     X = list()
     Y = list()
@@ -109,13 +42,13 @@ if __name__ == "__main__":
     sc = SparkContext()
     sc.setLogLevel("ERROR")
 
-    # folder_path = sys.argv[1]
-    # input_test_file = sys.argv[2]
-    # output_file = sys.argv[3]
+    folder_path = sys.argv[1]
+    input_test_file = sys.argv[2]
+    output_file = sys.argv[3]
 
-    folder_path = "data"
-    input_test_file = "data/yelp_val.csv"
-    output_file = "result/task.csv"
+    # folder_path = "data"
+    # input_test_file = "data/yelp_val.csv"
+    # output_file = "result/task.csv"
 
     #other input paths
 
@@ -123,10 +56,6 @@ if __name__ == "__main__":
     review_file = "/review_train.json"
     business_file = "/business.json"
     checkin_file = "/checkin.json"
-
-    DEFAULT_VALUE = 3.5
-    NEIGHBORS = 15
-    ALPHA = 0.0
 
     train_data_RDD = sc.textFile(folder_path + "/yelp_train.csv")
     header = train_data_RDD.first()
@@ -138,37 +67,6 @@ if __name__ == "__main__":
 
     business_user_dict = train_data_RDD.map(lambda row: (row[0], row[1])).groupByKey().mapValues(set).collectAsMap()
     user_business_dict = train_data_RDD.map(lambda row: (row[1], row[0])).groupByKey().mapValues(set).collectAsMap()
-    
-    ## COLLABORATIVE FILTERING: ITEM-BASED ##
-
-    business_avg_rating = train_data_RDD.filter(lambda row: row[2].replace('.', '', 1).isdigit()) \
-                                         .map(lambda x: (x[0], float(x[2]))) \
-                                         .groupByKey() \
-                                         .mapValues(lambda x: sum(x)/len(x)) \
-                                         .collectAsMap()
-
-    user_avg_rating = train_data_RDD.filter(lambda row: row[2].replace('.', '', 1).isdigit()) \
-                                         .map(lambda x: (x[1], float(x[2]))) \
-                                         .groupByKey() \
-                                         .mapValues(lambda x: sum(x)/len(x)) \
-                                         .collectAsMap()
-
-    bus_user_r = train_data_RDD.map(lambda row: (row[0], (row[1], row[2]))).groupByKey().mapValues(set).collect()
-
-    bus_user_r_dict = dict()
-    for bus, user_r_set in bus_user_r:
-        temp = dict()
-        for user_r in user_r_set:
-            temp[user_r[0]] = user_r[1]
-        bus_user_r_dict[bus] = temp
-    
-    weight_dict = dict()
-
-    cf_item_based_result = list()
-    for row in test_data_RDD.collect():
-        prediction = item_based_cf(row[0], row[1])
-        cf_item_based_result.append(prediction)
-
 
     ## MODEL-BASED ##
 
@@ -238,8 +136,7 @@ if __name__ == "__main__":
     ## HYBRID MODEL ##
     result_str = ""
     for i in range(len(model_based_result)):
-        result = float(ALPHA) * float(cf_item_based_result[i]) + (1 - float(ALPHA)) * float(model_based_result[i])
-        result_str += user_bus_list[i][0] + "," + user_bus_list[i][1] + "," + str(result) + "\n"
+        result_str += user_bus_list[i][0] + "," + user_bus_list[i][1] + "," + str(model_based_result[i]) + "\n"
 
     write_csv_file(result_str, output_file)
 
